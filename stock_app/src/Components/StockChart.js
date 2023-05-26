954ae43182c15dbc7aae2f718a49679a638e9770
Dzecki <oliwier.kostrzewski@wp.pl>
2023-05-26 12:50:19 +0200

    Update StockChart.js

--- stock_app/src/Components/StockChart.js
+++ stock_app/src/Components/StockChart.js
@@ -1,11 +1,16 @@
-import { Line } from "react-chartjs-2";
+import { useState } from "react";
+import { Line, Bar } from "react-chartjs-2";
 
 function StockChart({ chartData, firstRange, secondRange}) {
+  const [changeChart, setChangeChart] = useState(true);
+
   return (
-    <div className="relative border-2 rounded-lg p-4 w-[75vw] h-[520px]" >
+    <div className="relative border-2 rounded-lg p-8 w-[67vw] h-[520px]" >
 
     <p className="absolute top-2 right-2 text-white">{firstRange.substring(0, 4)} / {secondRange.substring(0, 4)}</p>
+    <button className="w-[25px] h-[25px] border-2 rounded-full" onClick={() => setChangeChart(changeChart ? false : true)}></button>
 
+    {changeChart ? (
       <Line
         data={chartData}
         options={{
@@ -20,6 +25,21 @@ function StockChart({ chartData, firstRange, secondRange}) {
           }
         }}
       />
+    ) : (
+      <Bar
+        data={chartData}
+        options={{
+          responsive: true,
+          plugins: {
+            title: {
+              display: true,
+            },
+            legend: {
+              display: false
+            }
+          }
+        }}
+      />)};
     </div>
   );
 };
